bb2510966f258b169df63426698ac7a77695980a
capricorncd <capricorncd@qq.com>
2024-10-24 23:07:18 +0900

    chore: Update

--- libs/demo/src/index.ts
+++ libs/demo/src/index.ts
@@ -1,64 +1,64 @@
-import { GuitarChords } from '@guitar-chords/canvas'
+import { GuitarChords as CanvasGuitarChords } from '@guitar-chords/canvas'
+import { GuitarChords as SvgGuitarChords } from '@guitar-chords/svg'
 
-  const app = document.getElementById('app') as HTMLDivElement
-  app.append(
-    new GuitarChords({
+const chordsList = [
+  {
     name: 'C',
     matrix: [
       [0, 0, 0, 0, 1, 0],
       [0, 0, 2, 0, 0, 0],
       [3, 4, 0, 0, 0, 0],
     ]
-  }).draw().element,
-  new GuitarChords({
+  },
+  {
     name: 'Dm',
     matrix: [
       [0, 0, 0, 0, 0, 1],
       [0, 0, 0, 2, 0, 0],
       [0, 0, 0, 0, 3, 0],
     ]
-  }).draw().element,
-  new GuitarChords({
+  },
+  {
     name: 'Em',
     matrix: [
       [0, 0, 0, 0, 0, 0],
       [0, 2, 3, 0, 0, 0],
       [0, 0, 0, 0, 0, 0],
     ]
-  }).draw().element,
-  new GuitarChords({
+  },
+  {
     name: 'F',
     matrix: [
       [1, 1, 0, 0, 0, 1],
       [0, 0, 0, 2, 0, 0],
       [0, 3, 4, 0, 0, 0],
     ]
-  }).draw().element,
-  new GuitarChords({
+  },
+  {
     name: 'G',
     matrix: [
       [0, 0, 0, 0, 0, 0],
       [0, 2, 0, 0, 0, 0],
       [3, 0, 0, 0, 0, 4],
     ]
-  }).draw().element,
-  new GuitarChords({
+  },
+  {
     name: 'Am',
     matrix: [
       [0, 0, 0, 0, 1, 0],
       [0, 0, 2, 3, 0, 0],
       [0, 0, 0, 0, 0, 0],
     ]
-  }).draw().element,
-  new GuitarChords({
+  },
+  {
     name: 'G7',
     matrix: [
       [0, 0, 0, 0, 0, 1],
       [0, 2, 0, 0, 0, 0],
       [3, 0, 0, 0, 0, 0],
     ]
-  }).draw().element,
-  new GuitarChords({
+  },
+  {
     name: 'C3',
     startFrets: 3,
     matrix: [
@@ -66,5 +66,14 @@ import { GuitarChords } from '@guitar-chords/canvas'
       [0, 0, 0, 0, 0, 0],
       [0, 0, 2, 3, 4, 0],
     ]
-  }).draw().element,
-)
+  }
+]
+
+  const app = document.getElementById('app') as HTMLDivElement
+  app.append(...chordsList.map(v => new CanvasGuitarChords(v).element))
+
+const h1 = document.createElement('h1')
+h1.textContent = 'SVG'
+app.append(h1)
+
+app.append(...chordsList.map(v => new SvgGuitarChords(v).element))
